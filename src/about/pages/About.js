9d81f90a50e3163594f2fd710539a13de2973ba6
Omkar Dhomane <47618402+dhomane@users.noreply.github.com>
2022-08-31 16:22:11 -0400

    Update About.js

--- src/about/pages/About.js
+++ src/about/pages/About.js
@@ -8,13 +8,13 @@ const About = () => {
 			<h2>About</h2>
 
 			<p>
-				This site is part of my portfolio: <a className="website-link" href="https://julienblondeau.com">julienblondeau.com</a>.
+				This site is part of my portfolio: <a className="website-link" href="https://julienblondeau.com">dhomane.com</a>.
 			</p>
 
 			<h2>Contact</h2>
 
-			<a className="contact-link" href="mailto:julienblondeaupro@protonmail.com">
-				julienblondeaupro@protonmail.com
+			<a className="contact-link" href="mailto:dhomane@gmail.com">
+				dhomane@gmail.com
 			</a>
 		</div>
 	);
